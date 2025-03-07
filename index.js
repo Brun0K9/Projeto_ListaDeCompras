cde7c883c06772938f44c3c7877dc9cf80997e45
Bruno Xavier <133142934+Brun0K9@users.noreply.github.com>
2025-03-07 10:23:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ import verificarListaVazia from "./scripts/verificarListaVazia.js";
 const listaDeCompras = document.getElementById("lista-de-compras");
 const botaoAdicionar = document.getElementById("adicionar-item");
 
-botaoAdicionar.addEventListener("click", (evento) => {
-  evento.preventDefault();
+botaoAdicionar.addEventListener("click", () => {
   const itemDaLista = criarItemDaLista();
   listaDeCompras.appendChild(itemDaLista);
   verificarListaVazia(listaDeCompras);
